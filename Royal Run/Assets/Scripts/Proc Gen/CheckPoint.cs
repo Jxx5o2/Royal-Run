b0f13d011056f017b0f7a7cdbc5d3effd56c00fa
이준서 <vtsun98@gmail.com>
2025-07-17 16:46:12 +0900

    CheckPoint 버그 수정

--- Royal Run/Assets/Scripts/Proc Gen/CheckPoint.cs
+++ Royal Run/Assets/Scripts/Proc Gen/CheckPoint.cs
@@ -3,22 +3,26 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
-    [SerializeField] float checkPointTimeExstension = 5f;
+    [SerializeField] int checkPointTimeExstension = 5;
 
     GameManager gameManager;
+    ScoreManager scoreManager;
 
-    const string playerString = "player";
+    const string playerString = "Player";
 
     void Start()
     {
         gameManager = FindFirstObjectByType<GameManager>();
+        scoreManager = FindFirstObjectByType<ScoreManager>();
     }
 
-    void OTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerString))
         {
             gameManager.IncreaseTime(checkPointTimeExstension);
+            scoreManager.IncreaseScore(checkPointTimeExstension);
+
         }
     }
 }
